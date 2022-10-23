dbc89129ee1e3a2bf264be3ba414b0d2624c1b79
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-10-23 13:00:07 -0300

    Finishes Client page and functionalities.

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,77 +1,183 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Cart.module.css';
+
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import {
+  PayPalButtons,
+  PayPalScriptProvider,
+  usePayPalScriptReducer,
+} from '@paypal/react-paypal-js';
+import { useEffect } from 'react';
+import { reset } from '../redux/cartSlice';
+
 function Cart() {
-  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
+  const [open, setOpen] = useState(false);
+  // This values are the props in the UI
+  const amount = cart.total;
+  const currency = 'BRL';
+  const style = { layout: 'vertical' };
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const createOrder = async (data) => {
+    try {
+      const res = await axios.post('http://localhost:3000/api/orders', data);
+      res.status === 201 && router.push('/orders/' + res.data._id);
+      dispatch(reset());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Custom component to wrap the PayPalButtons and handle currency changes
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+    // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+      dispatch({
+        type: 'resetOptions',
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
+    }, [currency, showSpinner]);
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function (details) {
+              const shipping = details.purchase_units[0].shipping;
+              console.log(shipping)
+              createOrder({
+                customer: shipping.name.full_name,
+                address: shipping.address.address_line_1,
+                total: cart.total,
+                method: 1,
+              });
+            });
+          }}
+        />
+      </>
+    );
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <table className={styles.table}>
-          <tr className={styles.trTitle}>
-            <th>Product</th>
-            <th>Name</th>
-            <th>Extras</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-          </tr>
-
-          {cart.products.map((product) => (
-            <tr className={styles.tr} key={product._id}>
-              <td>
-                <div className={styles.imgContainer}>
-                  <Image
-                    src={product.img}
-                    layout="fill"
-                    objectFit="cover"
-                    alt=""
-                  />
-                </div>
-              </td>
-              <td>
-                <span className={styles.name}>{product.title}</span>
-              </td>
-              <td>
-                <span className={styles.extras}>
-                  {product.extras.map(extra => 
-                    <span key={extra._id}>{extra.text},  </span>
-                    )}
-                  
-                </span>
-              </td>
-              <td>
-                <span className={styles.price}>${product.price}</span>
-              </td>
-              <td>
-                <span className={styles.quantity}>{product.quantity}</span>
-              </td>
-              <td>
-                <span className={styles.total}>
-                  ${product.price * product.quantity}
-                </span>
-              </td>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
             </tr>
-          ))}
+          </tbody>
+          <tbody>
+            {cart.products.map((product) => (
+              <tr className={styles.tr} key={product._id}>
+                <td>
+                  <div className={styles.imgContainer}>
+                    <Image
+                      src={product.img}
+                      layout="fill"
+                      objectFit="cover"
+                      alt=""
+                    />
+                  </div>
+                </td>
+                <td>
+                  <span className={styles.name}>{product.title}</span>
+                </td>
+                <td>
+                  <span className={styles.extras}>
+                    {product.extras.map((extra) => (
+                      <span key={extra._id}>{extra.text}, </span>
+                    ))}
+                  </span>
+                </td>
+                <td>
+                  <span className={styles.price}>${product.price}</span>
+                </td>
+                <td>
+                  <span className={styles.quantity}>{product.quantity}</span>
+                </td>
+                <td>
+                  <span className={styles.total}>
+                    ${product.price * product.quantity}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>CART TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Subtotal:</b>{cart.total}
+            <b className={styles.totalTextTitle}>Subtotal:</b>${cart.total}
           </div>
           <div className={styles.totalText}>
             <b className={styles.totalTextTitle}>Discount:</b>$0.00
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>{cart.total}
+            <b className={styles.totalTextTitle}>Total:</b>${cart.total}
           </div>
-          <button className={styles.button}>CHECKOUT NOW!</button>
+
+          {open ? (
+            <div className={styles.paymentMethods}>
+              <button className={styles.payButton}>CASH ON DELIVERY</button>
+              <PayPalScriptProvider
+                options={{
+                  'client-id':
+                    'AayB3ZABMM2b4keV5_63lyMPQeZFZN0gPCZk3vZ9UbbI7P-fmlfsJ9An1onOrcexnrpm2Tte6j5M6he5',
+                  components: 'buttons',
+                  currency: 'BRL',
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
+            </div>
+          ) : (
+            <button className={styles.button} onClick={() => setOpen(true)}>
+              CHECKOUT NOW!
+            </button>
+          )}
         </div>
       </div>
     </div>
--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,98 +1,132 @@
 import React from 'react';
 import styles from '../../styles/Order.module.css';
 import Image from 'next/image';
+import axios from 'axios';
 
-function Order() {
-
-  const status = 0;
+function Order({ order }) {
+  const status = order.status;
 
   const statusClass = (index) => {
-    if(index-status <1) return styles.done
-    if(index-status === 1) return styles.inProgress
-    if(index-status >1) return styles.undone
-  }
+    if (index - status < 1) return styles.done;
+    if (index - status === 1) return styles.inProgress;
+    if (index - status > 1) return styles.undone;
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.row}>
-        <table className={styles.table}>
-          <tr className={styles.trTitle}>
-            <th>Order ID</th>
-            <th>CUstomer</th>
-            <th>Address</th>
-            <th>Total</th>
-          </tr>
-          <tr className={styles.tr}>
-            <td>
-              <span className={styles.id}>1292312312348</span>
-            </td>
-            <td>
-              <span className={styles.name}>
-                Jhon Doe
-              </span>
-            </td>
-            <td>
-              <span className={styles.address}>Elton st. 212-33 LA</span>
-            </td>
-            <td>
-              <span className={styles.total}>$79.80</span>
-            </td>
-          </tr>
-        </table>
+          <table className={styles.table}>
+            <tbody>
+            <tr className={styles.trTitle}>
+              <th>Order ID</th>
+              <th>CUstomer</th>
+              <th>Address</th>
+              <th>Total</th>
+            </tr>
+            </tbody>
+            <tbody>
+            <tr className={styles.tr}>
+              <td>
+                <span className={styles.id}>{order._id}</span>
+              </td>
+              <td>
+                <span className={styles.name}>{order.customer}</span>
+              </td>
+              <td>
+                <span className={styles.address}>{order.address}</span>
+              </td>
+              <td>
+                <span className={styles.total}>${order.total}</span>
+              </td>
+            </tr>
+            </tbody>
+          </table>
         </div>
         <div className={styles.row}>
           <div className={statusClass(0)}>
-            <Image className={styles.checkedIcon} src="/img/paid.png" width={30} height={30} alt=""/>
+            <Image
+              className={styles.checkedIcon}
+              src="/img/paid.png"
+              width={30}
+              height={30}
+              alt=""
+            />
             <span>Payment</span>
             <div className={styles.checkedIcon}>
-            <Image className={styles.checkedIcon} src="/img/checked.png" width={20} height={20} alt=""/>
+              <Image
+                className={styles.checkedIcon}
+                src="/img/checked.png"
+                width={20}
+                height={20}
+                alt=""
+              />
             </div>
           </div>
 
           <div className={statusClass(1)}>
-            <Image className={styles.checkedIcon} src="/img/bake.png" width={30} height={30} alt=""/>
+            <Image
+              className={styles.checkedIcon}
+              src="/img/bake.png"
+              width={30}
+              height={30}
+              alt=""
+            />
             <span>Preparing</span>
             <div className={styles.checkedIcon}>
-            <Image className={styles.checkedIcon} src="/img/checked.png" width={20} height={20} alt=""/>
+              <Image
+                className={styles.checkedIcon}
+                src="/img/checked.png"
+                width={20}
+                height={20}
+                alt=""
+              />
             </div>
           </div>
 
           <div className={statusClass(2)}>
-            <Image src="/img/bike.png" width={30} height={30} alt=""/>
+            <Image src="/img/bike.png" width={30} height={30} alt="" />
             <span>On the way</span>
             <div className={styles.checkedIcon}>
-            <Image src="/img/checked.png" width={20} height={20} alt=""/>
+              <Image src="/img/checked.png" width={20} height={20} alt="" />
             </div>
           </div>
 
           <div className={statusClass(3)}>
-            <Image src="/img/delivered.png" width={30} height={30} alt=""/>
+            <Image src="/img/delivered.png" width={30} height={30} alt="" />
             <span>Delivered</span>
             <div className={styles.checkedIcon}>
-            <Image src="/img/checked.png" width={20} height={20} alt=""/>
+              <Image src="/img/checked.png" width={20} height={20} alt="" />
             </div>
           </div>
-
         </div>
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>CART TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+            <b className={styles.totalTextTitle}>Subtotal:</b>${order.total}
           </div>
           <div className={styles.totalText}>
             <b className={styles.totalTextTitle}>Discount:</b>$0.00
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>$79.60
+            <b className={styles.totalTextTitle}>Total:</b>${order.total}
           </div>
-          <button disabled className={styles.button}>PAID</button>
+          <button disabled className={styles.button}>
+            PAID
+          </button>
         </div>
       </div>
     </div>
   );
 }
+
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+  return {
+    props: { order: res.data },
+  };
+};
 
 export default Order;
